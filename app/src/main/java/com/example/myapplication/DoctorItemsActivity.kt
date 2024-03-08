1abf5bfb266bd79f9a5ed7260e86336fa65f18a3
Thisara <sasmithajayasinghe1@gmail.com>
2024-03-08 21:37:42 +0530

    completed doctorProfile page

--- app/src/main/java/com/example/myapplication/DoctorItemsActivity.kt
+++ app/src/main/java/com/example/myapplication/DoctorItemsActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DoctorItemsActivity : AppCompatActivity() {
+
+    lateinit var docProfileNav:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_items)
+
+        docProfileNav=findViewById(R.id.cardioDocOne)
+        docProfileNav.setOnClickListener {
+            val intent = Intent(this,DoctorProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
