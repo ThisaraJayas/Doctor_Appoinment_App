bd889c0dcf2a3bb3aef741204f81c83da0b542b3
Thisara <sasmithajayasinghe1@gmail.com>
2024-03-08 20:51:02 +0530

    completed doctorItems page

--- app/src/main/java/com/example/myapplication/CategoryActivity.kt
+++ app/src/main/java/com/example/myapplication/CategoryActivity.kt
@@ -1,11 +1,22 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class CategoryActivity : AppCompatActivity() {
+
+    lateinit var neuroCategory:CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        neuroCategory=findViewById(R.id.categoryOne)
+
+        neuroCategory.setOnClickListener {
+            val intent = Intent(this,DoctorItemsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
