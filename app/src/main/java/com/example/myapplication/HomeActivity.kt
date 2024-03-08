64a535589cde794c91d91a6fc6463ad956ce3fdb
Thisara <sasmithajayasinghe1@gmail.com>
2024-03-08 13:16:43 +0530

    completed profile page

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,11 +1,29 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var profileNavi: ImageView
+    lateinit var MedCategoryNavi:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        profileNavi=findViewById(R.id.profileNav)
+        MedCategoryNavi=findViewById(R.id.medCategoryNav)
+
+        profileNavi.setOnClickListener{
+            val intent = Intent(this,ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        MedCategoryNavi.setOnClickListener{
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
