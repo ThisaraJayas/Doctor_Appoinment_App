46c0f98012ff83b5d722b77cd35570d8dfb85e52
Thisara <sasmithajayasinghe1@gmail.com>
2024-03-08 13:53:37 +0530

    completed profileSuccess page

--- app/src/main/java/com/example/myapplication/ProfileActivity.kt
+++ app/src/main/java/com/example/myapplication/ProfileActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ProfileActivity : AppCompatActivity() {
+    lateinit var changeProfile:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        changeProfile=findViewById(R.id.profileChange)
+        changeProfile.setOnClickListener{
+            val intent = Intent(this, ProfileChangeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
