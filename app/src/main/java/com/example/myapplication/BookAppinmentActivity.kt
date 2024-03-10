8827157fa4f0f2873639cf1026836feabf580a53
Thisara <sasmithajayasinghe1@gmail.com>
2024-03-10 22:15:42 +0530

    completed frontend page

---
+++ app/src/main/java/com/example/myapplication/BookAppinmentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookAppinmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_appinment)
+    }
+}
